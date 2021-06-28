e7fcafbc9d99b6503da4f00eabac5108ae91828d
lifeshadow <abhishek.dhanani@bosctechlabs.com>
2021-06-28 20:03:39 +0530

    utils colors

---
+++ lib/widget/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const PrimaryBlueColor = Color(0xff36608F);
+const SecondaryGrayColor = Color(0xffE3E1E2);
+const TextWhiteColor = Color(0xffffffff);
+const TextBlackColor = Color(0xff000000);
