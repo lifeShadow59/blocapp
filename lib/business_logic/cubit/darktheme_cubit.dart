d8e0ec55577a88da40a69c4657c2410cf7c56ed4
lifeshadow <abhishek.dhanani@bosctechlabs.com>
2021-06-30 20:06:29 +0530

    dark theme store in mobile storage

--- lib/business_logic/cubit/darktheme_cubit.dart
+++ lib/business_logic/cubit/darktheme_cubit.dart
@@ -1,11 +1,23 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 part 'darktheme_state.dart';
 
-class DarkThemeCubit extends Cubit<DarkThemeState> {
-  DarkThemeCubit() : super(DarkThemeState(darkTheme: true));
+class DarkThemeCubit extends Cubit<DarkThemeState> with HydratedMixin {
+  DarkThemeCubit() : super(DarkThemeState(darkTheme: false));
 
   void changeTheme(bool darkTheme) =>
       emit(state.copyWith(darkTheme: darkTheme));
+
+  @override
+  DarkThemeState fromJson(Map<String, dynamic> json) {
+    return DarkThemeState.fromMap(json);
+  }
+
+  @override
+  Map<String, dynamic> toJson(DarkThemeState state) {
+    return state.toMap();
+  }
 }
--- lib/business_logic/cubit/darktheme_state.dart
+++ lib/business_logic/cubit/darktheme_state.dart
@@ -4,14 +4,31 @@ class DarkThemeState extends Equatable {
   final bool darkTheme;
   DarkThemeState({required this.darkTheme});
 
+  @override
+  List<Object> get props => [darkTheme];
+
   DarkThemeState copyWith({
-    required bool darkTheme,
+    bool? darkTheme,
   }) {
     return DarkThemeState(
-      darkTheme: darkTheme,
+      darkTheme: darkTheme ?? this.darkTheme,
     );
   }
 
-  @override
-  List<Object> get props => [darkTheme];
+  Map<String, dynamic> toMap() {
+    return {
+      'darkTheme': darkTheme,
+    };
+  }
+
+  factory DarkThemeState.fromMap(Map<String, dynamic> map) {
+    return DarkThemeState(
+      darkTheme: map['darkTheme'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory DarkThemeState.fromJson(String source) =>
+      DarkThemeState.fromMap(json.decode(source));
 }
